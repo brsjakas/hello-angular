2d99d1da0a6519aa9fa7fff7761b8a8d1ff725df
brsjakas <filip.brsjak@ambitioussolutions.mk>
2024-09-17 09:24:14 +0200

    chichin komit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from "@angular/core";
   selector: "app-root",
   standalone: true,
   imports: [],
-  template: ` <h1>Default APPLICATION</h1> `,
+  template: ` <h1>Default APPLICATION CHICHOOO</h1> `,
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
