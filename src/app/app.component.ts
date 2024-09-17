75aa0178e22e218c038355629ed99ac648160cb1
brsjakas <filip.brsjak@ambitioussolutions.mk>
2024-09-17 16:15:35 +0200

    updated title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from "@angular/core";
   selector: "app-root",
   standalone: true,
   imports: [],
-  template: ` <h1>Default APPLICATION CHICHOOO</h1> `,
+  template: ` <h1>Default APPLICATION AWS PIPELINE TEST</h1> `,
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
